e79b766f5910781184e646c80eac4487a605174a
anlige <zhanghuiguoanlige@126.com>
2021-07-17 15:42:16 +0800

    ；

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@
 
 import Loader from './bootloader'
 import Installer from './libs/installer'
+import qs from './libs/qs'
 import axios from 'axios'
 import './libs/promise.min'
 
 (function() {
   Loader.use(Installer)
+  window.qs = qs
   window.axios = axios
   window.vaspa = Loader
 })()
