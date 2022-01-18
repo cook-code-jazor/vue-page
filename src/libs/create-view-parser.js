6c88b3fd655c0f013d1c0fd23cf528d129465e4d
anlige <zhanghuiguoanlige@126.com>
2022-01-18 10:17:56 +0800

    ；

--- src/libs/create-view-parser.js
+++ src/libs/create-view-parser.js
@@ -1,14 +1,17 @@
 
 import { createComponent } from './cl'
-
-export default function (options = { viewRoot: '/', viewSuffix: '.html' }) {
+function viewParser (options = { viewRoot: '/', viewSuffix: '.html' }) {
   const viewRoot = options.viewRoot
   const viewSuffix = options.viewSuffix
   const ctor_ = function (path) {
     return viewRoot + path + viewSuffix
   }
-  ctor_.component = function (path) {
-    return createComponent(viewRoot + path + viewSuffix)
+  ctor_.component = function (path, absolute) {
+    return createComponent((absolute === true ? path : (viewRoot + path)) + viewSuffix)
+  }
+  ctor_.newInstance = function (options = { viewRoot: '/', viewSuffix: '.html' }) {
+    return viewParser(options)
   }
   return ctor_
 }
+export default viewParser
